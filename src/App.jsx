803d611685d85d72686c7a0e4bb553425ca5714b
Bilokon Yaroslav <uvill.github@gmail.com>
2025-02-02 17:38:12 +0200

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -17,64 +17,69 @@ export const goods = [
 
 export const App = () => {
   const [selectedGood, setSelected] = useState(8);
+  let key = 0;
 
   return (
     <main className="section container">
-      {selectedGood >= 0 ? (
+      {selectedGood === '' ? (
+        <h1 className="title is-flex is-align-items-center">
+          No goods selected
+        </h1>
+      ) : (
         <h1 className="title is-flex is-align-items-center">
           {goods[selectedGood]} is selected
           <button
             data-cy="ClearButton"
             type="button"
             className="delete ml-3"
-            onClick={() => setSelected(-1)}
+            onClick={() => setSelected('')}
           />
-        </h1>
-      ) : (
-        <h1 className="title is-flex is-align-items-center">
-          No goods selected
         </h1>
       )}
 
       <table className="table">
         <tbody>
-          {goods.map(good => (
-            <tr
-              data-cy="Good"
-              key={goods.indexOf(good)}
-              className={
-                selectedGood === goods.indexOf(good)
-                  ? 'has-background-success-light'
-                  : ''
-              }
-            >
-              <td>
-                {selectedGood === goods.indexOf(good) ? (
-                  <button
-                    data-cy="RemoveButton"
-                    type="button"
-                    className="button is-info"
-                    onClick={() => setSelected(-1)}
-                  >
-                    -
-                  </button>
-                ) : (
-                  <button
-                    data-cy="AddButton"
-                    type="button"
-                    className="button"
-                    onClick={() => setSelected(goods.indexOf(good))}
-                  >
-                    +
-                  </button>
-                )}
-              </td>
+          {goods.map(good => {
+            key += 1;
 
-              <td data-cy="GoodTitle" className="is-vcentered">
-                {good}
-              </td>
-            </tr>
-          ))}
+            return (
+              <tr
+                data-cy="Good"
+                key={key}
+                className={
+                  selectedGood === goods.indexOf(good)
+                    ? 'has-background-success-light'
+                    : ''
+                }
+              >
+                <td>
+                  {selectedGood === goods.indexOf(good) ? (
+                    <button
+                      data-cy="RemoveButton"
+                      type="button"
+                      className="button is-info"
+                      onClick={() => setSelected('')}
+                    >
+                      -
+                    </button>
+                  ) : (
+                    <button
+                      data-cy="AddButton"
+                      type="button"
+                      className="button"
+                      onClick={() => setSelected(goods.indexOf(good))}
+                    >
+                      +
+                    </button>
+                  )}
+                </td>
+
+                <td data-cy="GoodTitle" className="is-vcentered">
+                  {good}
+                </td>
+              </tr>
+            );
+          })}
         </tbody>
       </table>
     </main>
